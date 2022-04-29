515db491ba7014f8122720a4fd02fb46e73ac8ab
thoth2357 <98170427+thoth2357@users.noreply.github.com>
2022-04-29 11:55:07 +0100

    creating model checkpoint method

--- model_tuners.py
+++ model_tuners.py
@@ -1,13 +1,36 @@
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
+
 class callbacks():
     def __init__(self, monitor, patience):
         self.monitor = monitor
         self.patience = patience
 
-    def early_stopping():
+    def early_stopping(self):
         '''
-        argument: monitor, patience
+        argument: None
         purpose: Helps to stop model training when the generalization gap is getting worse.
-        
+        return: early_stopping object
         '''
-
-
+        stop = EarlyStopping(
+            monitor = self.monitor,
+            patience = self.patience
+        )
+        return stop
+    
+    def learning_reducer(self):
+        '''
+        argument: None
+        purpose: helps to reduce learning rate when a metric has stopped improving.
+        return: learning reducer object
+        '''
+        learning_reducer = ReduceLROnPlateau(
+            monitor = self.monitor,
+            patience = self.patience
+        )
+        return learning_reducer
+    
+    def model_checkpoint(self):
+        '''
+        argument: None
+        purpose: helps to save best performing models when an epoch increases the metrics ends
+        '''
