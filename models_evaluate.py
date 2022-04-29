c71f07fa2b571545d8b9eb4421b46fbbd75b0768
thoth2357 <98170427+thoth2357@users.noreply.github.com>
2022-04-29 15:16:32 +0100

    creating model evaluation class

---
+++ models_evaluate.py
@@ -0,0 +1,11 @@
+class Model_Evaluate():
+    
+    def plot_loss(trained_model, axis, xlabel, ylabel):
+        '''
+        arguments:trained_model, axis, xlabel, ylabel
+        purpose: plot the loss of the trained model against the epoch
+        return: None
+        '''
+        model_history = trained_model.history
+        model.his
+        
