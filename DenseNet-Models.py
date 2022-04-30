9e42242471c6dd2d412f6657eac610e8e79351da
thoth2357 <98170427+thoth2357@users.noreply.github.com>
2022-04-30 10:12:50 +0100

    finished the densenet model

--- DenseNet-Models.py
+++ DenseNet-Models.py
@@ -1,37 +1,37 @@
-from tensorflow.keras.models import Model
-from tensorflow.keras.applications.inception_v3 import InceptionV3
-from tensorflow.keras.utils import plot_model
+from tensorflow.keras.applications import DenseNet121
 from tensorflow.keras.layers import Input, Dense, Flatten, Dropout, BatchNormalization, Reshape, GlobalAveragePooling2D
+from tensorflow.keras.models import Model
 
-
-def model_create_and_train(data_preprocessor, callback, train_set, test_set):
+def model_create_and_train(model_type,data_preprocessor, callback, train_set, test_set):
     '''
-    argument:
-    purpose: Create Inference model with necessary hyperparameters
-    return: Created,compiled and trained Inference model
+    argument: 
+    purpose: Create DenseNet model with necessary hyperparameters
+    return: Created,compiled and trained Vgg model
     '''
 
-    model = InceptionV3(
-        input_shape = (150, 150, 3),
-        include_top = False,
-        weights = 'imagenet'
+    model = DenseNet121(
+        weights = 'imagenet',
+        include_top = False
     )
 
     for layer in model.layers:
-        layer.trainanle = False
-    
-    x = Flatten()(model.output)
-    x = Dense(1024, activation='relu')(x)
-    x = Dropout(0.2)(x)
+        layer.trainable = False
+
+    x = model.output()
+    x = GlobalAveragePooling2D()(x)
+    x = BatchNormalization()(x)
+
+    x = Dropout(0.5)(x)
 
     x = Dense(1, activation='sigmoid')(x)
+    predictions = Dense(1, activation='sigmoid')(x)
 
-    model_final = Model(inputs=model.input, output=x)
+    model_final = Model(inputs=model.input, outputs=predictions)
 
     checkpoint = callback.model_checkpoint(model_type = 'VGG19')
     learning_reducer = callback.learning_reducer()
     early_stop = callback.early_stopping()
-    
+
     compiled_model = callback.model_compiler(model_final)
 
     trained_model = compiled_model.fit(
@@ -45,4 +45,3 @@ def model_create_and_train(data_preprocessor, callback, train_set, test_set):
         callback = [checkpoint, learning_reducer, early_stop]
     )
     return trained_model
-
--- EfficientNet-models.py
+++ EfficientNet-models.py
@@ -1,8 +1,50 @@
-import efficientnet.keras as efn
+from efficientnet.keras import EfficientNetB0
+from tensorflow.keras.layers import Input, Dense, Flatten, Dropout, BatchNormalization, Reshape, GlobalAveragePooling2D
+from tensorflow.keras.models import Model
 
-def model_create_and_train(model_type,data_preprocessor, callback, train_set, test_set):
+def model_create_and_train(data_preprocessor, callback, train_set, test_set):
     '''
-    argument: model_type (which is going to be the type of Vgg model to work on either VGG19 or VGG 16)
+    argument: 
     purpose: Create VGG model with necessary hyperparameters
     return: Created,compiled and trained Vgg model
     '''
+    model = EfficientNetB0(
+        input_shape = (224, 224, 3),
+        include_top = False,
+        weights = 'imagenet'
+    )
+
+    for layer in model.layers:
+        layer.trainable = False
+    
+    x  = Flatten()(model.output)
+    # Add a fully connected layer with 512 hidden units and ReLU activation
+    x = Dense(1024, activation='relu')(x)
+
+    # Add a dropout rate of 0.5
+    x = Dropout(0.5)(x)
+
+    # Add a final sigmoid layer with 1 node for classification output
+    x = Dense(1, activation='sigmoid')(x)
+    predictions = Dense(1, activation='sigmoid')(x)
+
+    model_final = Model(inputs=model.input, outputs=predictions)
+    
+    checkpoint = callback.model_checkpoint(model_type = 'VGG19')
+    learning_reducer = callback.learning_reducer()
+    early_stop = callback.early_stopping()
+
+    compiled_model = callback.model_compiler(model_final)
+
+    trained_model = compiled_model.fit(
+        train_set,
+        epochs = data_preprocessor.EPOCHS,
+        steps_per_epoch = train_set.samples // data_preprocessor.BATCH_SIZE,
+        batch_size = data_preprocessor.batch_size,
+
+        validation_data = test_set,
+        validation_steps = test_set.samples // data_preprocessor.BATCH_SIZE - 10,
+        callback = [checkpoint, learning_reducer, early_stop]
+    )
+    return trained_model
+    
